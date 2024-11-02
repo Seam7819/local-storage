480b789cb48ba13e8a8de64dc8f7e84f12e4acbc
Snu Seam <siamahme766@gmail.com>
2024-11-02 13:48:45 +0600

    commit final project

--- local.js
+++ local.js
@@ -3,7 +3,43 @@ const handleBtn = () => {
     const product = inputField.value;
     const quantityField = document.getElementById('quantity-field');
     const quantity = quantityField.value;
-    quantityField.value = '' ;
-    inputField.value = '' ;
+    quantityField.value = '';
+    inputField.value = '';
     console.log(product,quantity);
+    displayProduct(product,quantity);
+    saveLocalStorage(product,quantity)
 }
+
+const displayProduct = (product,quantity) => {
+    const productContainer = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerText = `${product} : ${quantity}`
+    productContainer.appendChild(li);
+}
+
+const getStoredCart = () => {
+    let cart = {};
+    const storedCart = localStorage.getItem('cart');
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveLocalStorage = (product,quantity) =>{
+    const cart = getStoredCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+const displayProductFromLocalStorage = () => {
+    const products = getStoredCart()
+    console.log(products);
+    for(const product in products){
+        const quantity = products[product];
+        displayProduct(product,quantity)
+    }
+}
+
+displayProductFromLocalStorage()
